6fd48b291c5a3811c2ba0f42669af122ceae9176
Erdem <57351007+ErdemGns@users.noreply.github.com>
2022-04-18 22:33:59 +0300

    Update Run_SP3_read.bat

--- Run_SP3_read.bat
+++ Run_SP3_read.bat
@@ -1,46 +1,126 @@
 ECHO ON
 
+
 PRNfromSP3  G01  2022  03  01
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G02  2022  03  02
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G03  2022  03  03
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G04  2022  03  04
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G05  2022  03  05
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G06  2022  03  06
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G07  2022  03  07
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G08  2022  03  08
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G09  2022  03  09
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G10  2022  03  10
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G11  2022  03  11
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G12  2022  03  12
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G13  2022  03  13
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G14  2022  03  14
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G15  2022  03  15
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G16  2022  03  16
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G17  2022  03  17
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G18  2022  03  18
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G19  2022  03  19
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G20  2022  03  20
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G21  2022  03  21
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G22  2022  03  22
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G23  2022  03  23
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G24  2022  03  24
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G25  2022  03  25
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G26  2022  03  26
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G27  2022  03  27
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G28  2022  03  28
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G29  2022  03  29
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G30  2022  03  30
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G31  2022  03  31
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G01  2022  04  01
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G02  2022  04  02
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G03  2022  04  03
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G04  2022  04  04
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G05  2022  04  05
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G06  2022  04  06
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G07  2022  04  07
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G08  2022  04  08
+rename G01.xyz  G01_2022_03_01.txt
+
 PRNfromSP3  G09  2022  04  09
+rename G01.xyz  G01_2022_03_01.txt
+
 
 PAUSE
-
 
